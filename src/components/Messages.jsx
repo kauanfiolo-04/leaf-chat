208e2d1a10d9cf331d33b66fd8648d46717e908b
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-01-13 14:07:55 -0300

    aaaaaaaaaaaaaaa

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,7 +2,7 @@ import { Message } from "./Message"
 
 export const Messages=()=>{
   return(
-    <div className='bg-[#c1ddbe] p-[10px] h-[calc(100%-110px)]'>
+    <div className='bg-[#c1ddbe] p-[10px] h-[calc(100%-120px)]'>
       <Message/>
       <Message/>
       <Message/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Chat } from '../components/Chat'
 export const Home = () => {
   return (
     <div className='flex bg-lime-200 h-screen items-center justify-center'>
-      <div className='flex w-[65%] h-[85%] border-white rounded-lg border overflow-hidden'>
+      <div className='flex w-[65%] h-[85%] border-white rounded-lg border '>
         <Sidebar/>
         <Chat/>
       </div>
